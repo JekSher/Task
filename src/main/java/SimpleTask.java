70caa5e2a8edcb1448d0d8e9897af5046fd0cefd
Sher <jeksher.media@gmail.com>
2023-12-21 16:13:53 +0300

    add src/

--- src/main/java/SimpleTask.java
+++ src/main/java/SimpleTask.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SimpleTask {
+public class SimpleTask extends Task {
+    protected String title;
+
+    public SimpleTask(int id, String title) {
+        super(id); // вызов родительского конструктора
+        this.title = title; // заполнение своих полей
+    }
+
+    public SimpleTask(int id) {
+        super(id);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        if (title.contains(query)) {
+            return true;
+        }
+        return false;
+    }
 }
